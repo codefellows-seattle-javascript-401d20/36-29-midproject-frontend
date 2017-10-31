9b5052c014cac3de8581b7f74f6ead40bb6dc84f
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-30 22:08:33 -0700

    basic app with middleware running

--- lab-ron/src/main.js
+++ lab-ron/src/main.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 
+import reducer from './reducer'
 import App from './component/app'
+import thunk from './lib/redux-thunk.js'
+import reporter from './lib/redux-reporter.js'
+
+const store = createStore(reducer, applyMiddleware(thunk, reporter))
 
 let container = document.createElement('div')
 document.body.appendChild(container)
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store} >
     <App />
   </Provider>
   , container)
