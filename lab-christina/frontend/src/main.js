24ac685a4961e7e35c9557c9f532379271cd3537
Christina <penssake@gmail.com>
2017-10-31 09:44:27 -0700

    final frontend day one

--- lab-christina/frontend/src/main.js
+++ lab-christina/frontend/src/main.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
-// import reducer from '/.reducer'
-// import App from './component/app'
-// import thunk from './lib/redux-thunk.js'
-// import reporter from './lib/redux-reporter.js'
+import reducer from '/.reducer'
+import App from './component/app'
+import thunk from './lib/redux-thunk.js'
+import reporter from './lib/redux-reporter.js'
 
-// const store = createStore(reducer, applyMiddleware(thunk, reporter))
+const store = createStore(reducer, applyMiddleware(thunk, reporter))
 
 let container = document.createElement('div')
 document.body.appendChild(container)
