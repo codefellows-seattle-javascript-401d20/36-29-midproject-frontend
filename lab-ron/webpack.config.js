bfad8f9643b35eada8d374ddda88ed32c7877b80
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-30 20:55:03 -0700

    configging the webpack

---
+++ lab-ron/webpack.config.js
@@ -0,0 +1,50 @@
+'use strict'
+
+
+
+const HTMLPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const {DefinePlugin, EnvironmentPlugin} = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: `${__dirname}/src/main.js`,
+  devServer: {
+    historyApiFallback: true,
+  },
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/build`,
+  },
+  plugins: [
+    new HTMLPlugin({title: 'Budget Tracker'}),
+    new ExtractPlugin('bundle.[hash].css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract({
+          use: [
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                includePaths: [`${__dirname}/src/style`],
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  },
+}
