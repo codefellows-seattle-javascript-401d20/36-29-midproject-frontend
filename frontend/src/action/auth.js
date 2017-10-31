c4d7f386d7d54d77c24da0e09bcbb47658a476e1
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-30 20:43:46 -0700

    frontend and backend coded out

---
+++ frontend/src/action/auth.js
@@ -0,0 +1,30 @@
+import superagent from 'superagent';
+
+export const tokenSet = (token) => ({
+  type: 'TOKEN_SET',
+  payload: token,
+});
+
+export const tokenRemove = () => ({
+  type: 'TOKEN_REMOVE',
+});
+
+export const signup = (user) => (store) => {
+  return superagent.post(`${__API_URL__}/signup`)
+  .send(user)
+  .withCredentials()
+  .then(res => {
+    console.log({res});
+    return store.dispatch(tokenSet(res.text));
+  });
+};
+
+export const login = (user) => (store) => {
+  return superagent.get(`${__API_URL__}/login`)
+  .auth(user.username, user.password)
+  .withCredentials()
+  .then(res => {
+    console.log({res});
+    return store.dispatch(tokenSet(res.text));
+  });
+};
