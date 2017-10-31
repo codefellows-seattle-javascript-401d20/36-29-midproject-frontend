69cf293d8a2a923bba0992717a62c0f320789964
mackoyokcam <mark.hangarter@gmail.com>
2017-10-31 10:08:35 -0700

    fixed backend issue of not setting cookie

--- lab-mark/frontend/src/component/auth-redirect/index.js
+++ lab-mark/frontend/src/component/auth-redirect/index.js
@@ -9,7 +9,7 @@ class AuthRedirect extends React.Component {
     let {pathname} = location
     let to = null
 
-    if(pathname == '/login' || pathname === '/signup' || pathname === '/'){
+    if(pathname === '/login' || pathname === '/signup' || pathname === '/'){
       if(token){
         to = '/dashboard'
       }
