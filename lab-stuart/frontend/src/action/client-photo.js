249b47d5963b8ada63ae62cefbdb8a846903e980
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-11-03 08:38:39 -0700

    Avatar upload underway

---
+++ lab-stuart/frontend/src/action/client-photo.js
@@ -0,0 +1,45 @@
+import superagent from 'superagent';
+
+export const set = (photos) => ({
+  type: 'CLIENT_PHOTOS_SET',
+  payload: photos,
+});
+
+export const create = (photo) => ({
+  type: 'CLIENT_PHOTO_CREATE',
+  payload: photo,
+});
+
+export const remove = (photo) => ({
+  type: 'CLIENT_PHOTO_REMOVE',
+  payload: photo,
+});
+
+export const fetchRequest = () => (store) => {
+  let {token} = store.getState();
+  return superagent.get(`${__API_URL__}/profiles/avatar`)
+    .set('Authorization', `Bearer ${token}`)
+    .then(res => {
+      return store.dispatch(set(res.body.data));
+    });
+};
+
+export const createRequest = (photo) => (store) => {
+  let {token} = store.getState();
+  return superagent.post(`${__API_URL__}/profiles/avatar`)
+    .set('Authorization', `Bearer ${token}`)
+    .field('description', photo.description)
+    .attach('photo', photo.photo)
+    .then(res => {
+      return store.dispatch(create(res.body));
+    });
+};
+
+// export const removeRequest = (photo) => (store) => {
+//   let {token} = store.getState();
+//   return superagent.delete(`${__API_URL__}/profiles/avatar/${photo._id}`)
+//     .set('Authorization', `Bearer ${token}`)
+//     .then(() => {
+//       return store.dispatch(remove(photo));
+//     });
+// };
