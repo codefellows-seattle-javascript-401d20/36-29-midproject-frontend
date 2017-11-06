8a4861f03bba738fd05ef66cc1febeefdb1957db
Christina <penssake@gmail.com>
2017-11-06 07:29:44 -0800

    README updated

--- lab-christina/backend/route/auth-router.js
+++ lab-christina/backend/route/auth-router.js
@@ -7,31 +7,34 @@ const basicAuth = require('../lib/basic-auth-middleware.js');
 
 const authRouter = module.exports = new Router();
 
-authRouter.post('/auth', (req, res, next) => {
+authRouter.post('/auth', (req, response, next) => {
   Account.create(req.body)
     .then(account => account.tokenCreate())
     .then(token => {
-      res.json({token});
+      response.cookie('charity-choice-Token', token, {maxAge:604800000});
+      console.log('BACKEDND REQUEST', token);
+      response.json({token});
     })
     .catch(next);
 });
 
-authRouter.get('/auth', basicAuth, (req, res, next) => {
+authRouter.get('/auth', basicAuth, (req, response, next) => {
   req.account.tokenCreate()
     .then(token => {
-      res.json({token});
+      response.cookie('charity-choice-Token', token, {maxAge:604800000});
+      response.json({token});
     })
     .catch(next);
 });
 
-authRouter.put('/auth', basicAuth, (req, res, next) => {
+authRouter.put('/auth', basicAuth, (req, response, next) => {
 
   if (!req.body.username || !req.body.email || !req.body.password)
     return next(httpErrors(400, '__REQUEST_ERROR__ username, email, and password required'));
 
   req.account.update(req.body)
     .then(() => {
-      res.sendStatus(200);
+      response.sendStatus(200);
     })
     .catch(next);
 });
---
+++ lab-christina/frontend/README.md
@@ -0,0 +1,21 @@
+#  Charity Choice front-end
+
+## About:
+Lab 36-39 - Assignment was to build a front end for our mid-project week API.
+
+### entry
+main.js
+
+### Components
+```
+Provider
+  App
+    AuthRedirect
+    Landing
+      // handle login and signup
+      //allows user to sign up and login to the charity choice application
+      //user is only allowed to remain on landing route if they are not logged in.
+    Dashboard
+      // display main app
+
+```
